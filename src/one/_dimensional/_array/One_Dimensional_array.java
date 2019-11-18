7764cb10724b1ea43408e5c3efabb07430b1a8fc
AMINUL-IT-SOLUTIONS <AMINUL-IT-SOLUTIONS@DESKTOP-E2NQQN2>
2019-11-18 13:15:14 -0800

    9th commit

---
+++ src/one/_dimensional/_array/One_Dimensional_array.java
@@ -0,0 +1,15 @@
+
+package one._dimensional._array;
+
+
+public class One_Dimensional_array {
+
+    public static void main(String[] args) {
+        int[]number=new int[20];
+        for(int n=0;n<=number.length;n++){
+            number[n]=n+1;
+            System.out.println(number[n]);
+        }
+    }
+    
+}
